447d58eb3d2b784ef560464fb53b32f6cb40dd19
Aneeq-Younas <85731812+Aneeq-Younas@users.noreply.github.com>
2021-09-13 23:04:27 -0700

    Add files via upload

---
+++ val.py
@@ -0,0 +1,11 @@
+def number(letters, nmbr):
+    for i in letters:
+        if nmbr==i:
+            return True
+        return False
+print(number([2,44,3,22,11,14,35,55,66], 2))
+print(number([11,22,33,44,8 ], 1))
+
+
+
+
